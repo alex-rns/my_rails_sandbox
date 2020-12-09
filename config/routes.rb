b5c9926b88dfc37b976e4d5645523d1da5cf1aa1
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-10 00:31:17 +0200

    Settled on point 6 of guides: https://guides.rubyonrails.org/getting_started.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about', as: 'about'
 
+  get "/articles", to: "articles#index"
+
   resources :posts do
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_191031) do
+ActiveRecord::Schema.define(version: 2020_12_09_215605) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_12_07_191031) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
