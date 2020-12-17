bcdddd3beb2a18250a280aa0beb7b31a177211cc
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-17 23:15:39 +0200

    Check post validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: {minimum: 5}, uniqueness: true
+  validates :body, length: {in: 5..50}, uniqueness: true
 end
